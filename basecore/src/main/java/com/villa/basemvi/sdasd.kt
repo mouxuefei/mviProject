ac7fde7725a0c897d7247f68c77c0f9cf9ff1f96
villa_mou <mouxuefei@edocyun.com.cn>
2023-04-21 10:22:59 +0800

    feat:init

---
+++ basecore/src/main/java/com/villa/basemvi/sdasd.kt
@@ -0,0 +1,10 @@
+package com.villa.basemvi
+
+/**
+ * @author: villa_mou
+ * @date: 2023-02-27:14
+ * @version V1.0
+ * @desc <描述当前版本功能>
+ */
+class sdasd {
+}
---
+++ basenet/src/main/java/com/villa/basenet/aaa.java
@@ -0,0 +1,10 @@
+package com.villa.basenet;
+
+/**
+ * @version V1.0
+ * @author: villa_mou
+ * @date: 2023-02-27:14
+ * @desc <描述当前版本功能>
+ */
+class aaa {
+}
